128044471db382a9499b4a94a1b8c8642b85fa4d
crssnky <vindicare28@yahoo.co.jp>
2018-10-13 23:46:58 +0900

    add GeoTIFF Support CheckFlag

--- Source/GeoTIFF_Importer/Private/GeoTIFF_Importer.cpp
+++ Source/GeoTIFF_Importer/Private/GeoTIFF_Importer.cpp
@@ -5,12 +5,14 @@
 #include "IGeoTIFF_Importer.h"
 
 #include "LevelEditor.h"
+#include "LandscapeEditorModule.h"
 #include "MultiBoxBuilder.h"
 #include "SlateApplication.h"
 #include "SButton.h"
 #include "IMainFrameModule.h"
 #include "SWindow.h"
 #include "DesktopPlatformModule.h"
+#include "LandscapeFileFormatGeoTIFF.h"
 
 #define LOCTEXT_NAMESPACE "crssnkyPlugin"
 
@@ -21,19 +23,21 @@ class FGeoTIFF_Importer: public IGeoTIFF_Importer{
 	virtual void ShutdownModule() override;
 	void OnWindowMenuExtension(FMenuBuilder& MenuBuilder);
 	void OnMyToolMenu();
+	void OnChecked();
+	void OnUnChecked();
 	TSharedPtr<FExtender> Extender;
+	TSharedPtr<FLandscapeHeightmapFileFormat_GeoTIFF> GeoTIFF_Importer;
 	bool b = false;
 };
 
 IMPLEMENT_MODULE(FGeoTIFF_Importer, GeoTIFF_Importer)
-
-
 
 void FGeoTIFF_Importer::StartupModule(){
 	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
 	UE_LOG(LogTemp, Display, TEXT("Loaded GeoTIFF_Importer"));
 	if(IsRunningCommandlet()){ return; }
 
+	GeoTIFF_Importer = MakeShareable(new FLandscapeHeightmapFileFormat_GeoTIFF());
 	Extender = MakeShareable(new FExtender);
 	Extender->AddMenuExtension(
 		"LevelEditor",
@@ -80,6 +84,27 @@ void FGeoTIFF_Importer::OnWindowMenuExtension(FMenuBuilder & MenuBuilder){
 
 void FGeoTIFF_Importer::OnMyToolMenu(){
 	b = !b;
+	if(b){
+		UE_LOG(LogTemp, Display, TEXT("GeoTIFF_Importer true."));
+		OnChecked();
+	} else{
+		UE_LOG(LogTemp, Display, TEXT("GeoTIFF_Importer false."));
+		OnUnChecked();
+	}
+}
 
-	UE_LOG(LogTemp, Warning, TEXT("いべんとだよ～, %d"), StaticCast<int>(b));
+void FGeoTIFF_Importer::OnChecked(){
+	ILandscapeEditorModule& LEM = FModuleManager::GetModuleChecked<ILandscapeEditorModule>("LandscapeEditor");
+	FString extensions = LEM.GetHeightmapImportDialogTypeString();
+	if(!extensions.Contains(".tif")){
+		LEM.RegisterHeightmapFileFormat(GeoTIFF_Importer.ToSharedRef());
+	}
+}
+
+void FGeoTIFF_Importer::OnUnChecked(){
+	ILandscapeEditorModule& LEM = FModuleManager::GetModuleChecked<ILandscapeEditorModule>("LandscapeEditor");
+	FString extensions = LEM.GetHeightmapImportDialogTypeString();
+	if(extensions.Contains(".tif")){
+		LEM.UnregisterHeightmapFileFormat(GeoTIFF_Importer.ToSharedRef());
+	}
 }
